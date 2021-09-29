17e82fbe76c5d739701f8de89e2f87c40e206f05
libbyleonard22 <815989@mystma.org>
2021-09-29 15:08:53 -0500

    5

--- main.py
+++ main.py
@@ -1,22 +1,25 @@
 #WRITE YOUR CODE IN THIS FILE
 
+def greaterThan(x, y):
+    if   (4, 8):
+        return True
+
+def lessThan(x, y):
+    if   (33, 18):
+        return False
+
+def equalTo(x, y):
+    if   (90, 34):
+        return True
+
+def greaterOrEqual(x, y):
+    if   (22, 44):
+        return False
+
+def lessOrEqual(x, y):
+    if (12, 32):
+        return True
+
 def function(x, y):
-    if greaterThan(22,50):
-        return True
-
-    elif   lessThan(33, 18):
+    if (5, 7):
         return False
-
-    elif   equalTo(90,34):
-        return True
-
-    elif   greaterOrEqual(22, 44):
-        return False
-
-    elif lessOrEqual(12, 32):
-        return True
-
-    else:
-        return False
-
-print(function(x, y))
